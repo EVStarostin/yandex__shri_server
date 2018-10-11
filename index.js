8c073c4730adfaba2bdc75c33ab47179c80bfd4c
Evgenii Starostin <e.starostin89@gmail.com>
2018-10-11 21:45:17 +0300

    перевел на post и сделал пагинацию

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const fs = require('fs');
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/status', function (req, res) {
   let uptime = process.uptime();
@@ -13,21 +14,44 @@ app.get('/status', function (req, res) {
 });
 
 app.get('/api/events', function (req, res) {
-  const { type } = req.query;
+  const { type, page, limit } = req.query;
   const types = ['info', 'critical'];
   if (type && types.indexOf(type) < 0) {
-    res.status(400).send({ status: 'error', message: 'incorrect type' });
+    res.status(400).json({ msg: 'incorrect type' });
     return;
   }
   fs.readFile('./events.json', (err, data) => {
     if (err) throw err;
+    let { events } = JSON.parse(data);
+    let paginatedEvents = [];
     if (!type) {
-      res.send(JSON.parse(data));
+      paginatedEvents = paginate(events, Number(page), Number(limit));
     } else {
-      let { events } = JSON.parse(data);
       events = events.filter(event => event.type === type);
-      res.send({ events });
+      paginatedEvents = paginate(events, Number(page), Number(limit));
     }
+    res.json({ events: paginatedEvents, total: events.length });
+  });
+});
+
+app.post('/api/events', function (req, res) {
+  const { type, page, limit } = req.body;
+  const types = ['info', 'critical'];
+  if (type && types.indexOf(type) < 0) {
+    res.status(400).json({ msg: 'incorrect type' });
+    return;
+  }
+  fs.readFile('./events.json', (err, data) => {
+    if (err) throw err;
+    let { events } = JSON.parse(data);
+    let paginatedEvents = [];
+    if (!type) {
+      paginatedEvents = paginate(events, Number(page), Number(limit));
+    } else {
+      events = events.filter(event => event.type === type);
+      paginatedEvents = paginate(events, Number(page), Number(limit));
+    }
+    res.json({ events: paginatedEvents, total: events.length });
   });
 });
 
@@ -45,11 +69,26 @@ function formatTime(time) {
   const seconds = Math.floor(time % 3600 % 60);
   const formattedTime = `${
     (hours / 10 < 1) ? '0' + hours : hours
-  }:${
+    }:${
     (minutes / 10 < 1) ? '0' + minutes : minutes
-  }:${
+    }:${
     (seconds / 10 < 1) ? '0' + seconds : seconds
-  }`;
-  
+    }`;
+
   return formattedTime;
 };
+
+function paginate(array, page, limit) {
+  const defaultLimit = 10;
+  let respondData = [];
+  if (Number.isFinite(page) && Number.isFinite(limit)) {
+    respondData = array.slice( (page - 1) * limit, page * limit );
+  } else if (Number.isFinite(page)) {
+    respondData = array.slice( (page - 1) * defaultLimit, page * defaultLimit );
+  } else if (Number.isFinite(limit)) {
+    respondData = array.slice( 0, limit );
+  } else {
+    respondData = array;
+  }
+  return respondData;
+}
