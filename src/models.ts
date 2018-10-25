57b5d8c1234dfc4830aa0579ca4b2516239d2a03
Evgenii Starostin <e.starostin89@gmail.com>
2018-10-25 22:28:06 +0300

    перевел приложение на type script

---
+++ src/models.ts
@@ -0,0 +1,49 @@
+export interface IData {
+  events: IEvent[];
+  total: number;
+}
+
+export interface IEvent {
+  type: string;
+  title: string;
+  source: string;
+  time: string;
+  description: string;
+  icon: string;
+  size: string;
+  data: IGraphData & IButtonsData & IImageData & ITemperatureData & IMusicData;
+}
+
+interface IGraphData {
+  type: string;
+  values: IGraphValues[];
+}
+
+interface IGraphValues {
+  electricity: (number | string)[];
+  water: (number | string)[];
+  gas: (number | string)[];
+}
+
+interface IButtonsData {
+  buttons: string[];
+}
+
+interface IImageData {
+  image: string;
+}
+
+interface ITemperatureData {
+  temperature: number;
+  humidity: string;
+}
+
+interface IMusicData {
+  albumcover: string;
+  artist: string;
+  track: {
+    name: string;
+    length: string;
+  };
+  volume: number;
+}
